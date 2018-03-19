99b049db94869f586ed8de1fbd4a8ced9ca98cc0
Loran Kary <lk@focalpnt.com>
2018-03-19 10:28:03 -0700

    Initial commit

---
+++ canvasDemo.js
@@ -0,0 +1,243 @@
+window.onload = init; // Wait for the page to load before we begin animation
+var renderer;
+var scene;
+var camera;
+var balls = [];
+var num_balls = 20;
+var canvas;
+
+function init(){
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( 900, 600 );   // the canvas size
+    document.body.appendChild( renderer.domElement );
+    canvas = renderer.domElement; //get the canvas
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color("rgb(96, 96, 96)");
+
+    // White directional light at full intensity shining from directly above.
+    var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
+    // directionalLight.position.x = canvas.width/2;
+    // directionalLight.position.y = -canvas.height/2;
+    directionalLight.position.z = 100;
+    scene.add( directionalLight );
+
+
+    // A camera angle of view of 30 degrees and a z position of
+    // 1130 approximates one scene coordinate unit to
+    // one pixel.
+    camera = new THREE.PerspectiveCamera( 30,
+        canvas.width/canvas.height, 100, 2000 );
+	camera.position.z = 1130;
+    // Move the camera so that the origin of the coordinates
+    // is in the bottom left of the camera's frustrum
+    // rather than the center.  Threejs has positive y values
+    // going up rather than down so the origin should be
+    // lower left rather than upper right.
+    camera.position.x = canvas.width/2;
+    camera.position.y = canvas.height/2;
+
+
+    // Set the dimensions of the canvas
+    canvas.style.marginTop = canvas.height * 0.08 + 'px';
+    canvas.style.marginBottom = canvas.height * 0.08 + 'px';
+    canvas.style.marginRight = canvas.width * 0.08 + 'px';
+    canvas.style.marginLeft = canvas.width * 0.08 + 'px';
+    canvas.style.border = 'solid black 2px';
+
+    // create array of balls where no ball is created overlapping
+    // any other ball.
+    for (var i = 0; i < num_balls; i++){
+       var ball;
+     while(true) {
+         var radius = Math.random()*20 + 15;
+         var color = randomColor();
+         //set location vector
+         var x = Math.random() * (canvas.width-2*radius) + radius;
+         var y = Math.random() * (canvas.height-2*radius) + radius;
+         var loc = new vector2d(x, y);
+         //set velocity vector
+         var r = (Math.random()* 4 + 0.5);
+         var theta = Math.random() * 2 * Math.PI;
+         var vel = new vector2d(undefined, undefined, r, theta);
+         var acc = new vector2d(0, 0);
+         ball = new Mover(radius, loc, vel, acc, color);
+         // check that this new ball does not collide with any other ball
+         for(var j = 0;  j < balls.length; j++){
+             if(vector2d.distance(balls[j].loc,ball.loc) <= (balls[j].radius + ball.radius))
+                 break;  // collision
+         }
+         if(j === balls.length)
+             break;  // no collision
+         }
+
+     balls[i] = ball;
+     scene.add(ball.mesh);
+    }
+    logEnergy();
+    animate();
+}
+
+function animate() {
+    requestAnimationFrame( animate );
+    checkBallBounces();
+    for(let i = 0; i < balls.length; i++)
+        balls[i].update();
+    renderer.render(scene, camera);
+};
+
+
+//
+function logEnergy() {
+    console.log(`totalKineticEnergy ${totalKineticEnergy().toFixed(2)}`);
+    setTimeout(logEnergy, 5000);  // five seconds
+}
+
+function totalKineticEnergy() {
+  var sum = 0;
+  for(var i = 0; i < balls.length; i++){
+    sum += balls[i].kineticEnergy();
+  }
+  return sum;
+}
+
+//returns a random pastel color
+function randomColor(){
+  let red = Math.floor(Math.random()*64 + 192);
+  let blue = Math.floor(Math.random()*64 + 192);
+  let green = Math.floor(Math.random()*64 + 192);
+  let pastel = `rgb(${red},${blue},${green})`
+  return pastel;
+}
+
+// Check if balls should bounce off each other.
+// This where we have the collision code.
+// A collision between two balls is not so much between the
+// the two balls as it is between each ball and the
+// center of mass (cm) which has its own velocity.
+// The velocities of each ball and the cm can be broken down
+// into two components where one component is in the direction
+// of the collision and one component is orthogonal to the
+// collision.  Each orthogonal component contributes no energy
+// to the collision but the energy that it does have must be
+// conserved.
+// The center of mass and the collision itself all lie on a
+// line connecting the center of the two colliding bodies.
+// The laws of physics say that the total momentum (mass * velocity)
+// and the total kinetic energy (1/2 mass * velocity * velocity)
+// after an elastic collision must be the same as before the collision.
+
+function checkBallBounces () {
+
+  for(var i = 0; i < balls.length ; i++){
+    for(var j = i + 1; j < balls.length; j++){
+
+      //check if edges of 2 balls are touching
+      var dist = vector2d.distance(balls[i].loc, balls[j].loc);
+      if( dist <= balls[i].radius + balls[j].radius){
+        var b1 = balls[i];      // ball one
+        var b2 = balls[j];      // ball two
+
+        // sometimes the balls will stick together if there is
+        // too much overlap initially.  So separate them enough
+        // that they are just touching.
+        var vec = vector2d.subtract(b1.loc, b2.loc);
+        vec.setMag((b1.radius+b2.radius - vec.magnitude())/2);
+        b1.loc.add(vec);
+        vec.scalarMult(-1);
+        b2.loc.add(vec);
+
+        // momentum & velocity of center of mass
+        // find the total initial momentum (p) before the collision
+        var p_initial = vector2d.add(b1.momentum(), b2.momentum());
+        var total_mass = b1.mass + b2.mass;
+        // The velocity of the center of mass (vel_cm) is the
+        // total momentum divided the total mass.
+        var vel_cm = vector2d.scalarDiv(p_initial, total_mass);
+
+        // To solve for the final velocities of ball one and ball two,
+        // we need to find the following things.
+        // 1. The component of ball one's initial velocity (vel_b1) in the direction
+        // of the center of mass (vel_b1_cm).
+        // 2. The component of ball two's initial velocity (vel_b2) in the direction
+        // of the center of mass (vel_b2_cm).
+        // 3. The component of the center of mass's velocity in the direction
+        // of b1 (vel_cm_b1), which is also the component of the center
+        // of mass's velocity in the direction of b2.
+        // 4. The component of ball one's initial velocity orthogonal to the direction
+        // of the center of mass (vel_b1_not_cm)
+        // 5. The component of ball two's initial velocity orthogonal to the direction
+        // of the center of mass (vel_b2_not_cm)
+        //
+        // vel_b1_cm is the magnitude of ball one's initial velocity times
+        // the cosine of the angle between its velocity and the direction
+        // to the center of mass.  vel_b1_not_cm is the sine of the angle
+        // between or the difference between ball one's initial velocity
+        // and vel_b1_cm.
+        // vel_b2_cm is the magnitude of ball two's initial velocity times
+        // the cosine of the angle between its velocity and the direction
+        // of the center of mass.  vel_b2_not_cm is the sine of the angle
+        // between or the difference between ball two's initial velocity
+        // and vel_b2_cm.
+        // vel_cm_b1 is the magnitude of the center of mass's velocity times
+        // the cosine of the angle between its velocity and the direction
+        // of ball one.
+
+
+        // Where is the center of mass?  It must lie on a line
+        // connecting the two colliding objects with a magnitude
+        // proportionate to the two masses.
+        var vec_cm = vector2d.subtract(b1.loc, b2.loc);
+        vec_cm.setMag(vec_cm.magnitude() * (b1.mass/(b1.mass+b2.mass)));
+        vec_cm.add(b2.loc); // location of CM
+
+        // For each ball, what is the component of its velocity towards
+        // the center of mass and what is the component that is not
+        // in the direction of the center of mass?
+
+        // get a vector from  ball 1 to the center of mass
+        var vel_b1_cm = vector2d.subtract(vec_cm, b1.loc);
+        // its magnitude should be the magnitude of the balls velocity
+        // times the cosine of the angle between itself and the
+        // velocity of the ball
+        // component of b1 velocity on a line to the CM
+        vel_b1_cm.setMag(b1.vel.magnitude() * Math.cos(vector2d.angleBetween(b1.vel,vel_b1_cm)));
+        // component of the CM's velocity on a line towards b1
+        var vel_cm_b1 = vector2d.copy(vel_b1_cm); // on the same line as vel_b1_cm
+        vel_cm_b1.setMag(vel_cm.magnitude() * Math.cos(vector2d.angleBetween(vel_cm,vel_cm_b1)));
+        // The component of the ball's velocity not in the direction of the
+        // center of mass should be the difference between its total velocity
+        // and the component in the direction of the center of mass
+        var vel_b1_not_cm = vector2d.subtract(b1.vel,vel_b1_cm);
+
+        // Now repeat for the second ball
+        var vel_b2_cm = vector2d.subtract(vec_cm, b2.loc);
+        vel_b2_cm.setMag(b2.vel.magnitude() * Math.cos(vector2d.angleBetween(b2.vel,vel_b2_cm)));
+        var vel_b2_not_cm = vector2d.subtract(b2.vel,vel_b2_cm);
+
+        //calculate velocities after collision using vf = 2*v_cm - vi
+        //http://courses.ncssm.edu/apb11o/resources/guides/G09-4b.com.htm
+
+        var v1_final = vector2d.scalarMult(vel_cm_b1, 2); // 2 times the velocity of cm
+        v1_final.subtract(vel_b1_cm);   // subtract b1's velocity towards the CM
+        v1_final.add(vel_b1_not_cm);    // add back b1's velocity not towards the CM
+        // console.log(`b1 final velocity ${v1_final}`);
+
+        var v2_final = vector2d.scalarMult(vel_cm_b1, 2);
+        v2_final.subtract(vel_b2_cm);   // subtract velocity towards the CM
+        v2_final.add(vel_b2_not_cm);    // add back the velocity not towards the CM
+
+
+        b1.vel = v1_final;
+        b2.vel = v2_final;
+
+        // note the total momentum after the collision
+        // which should be equal to p_initial
+        var p_final = vector2d.add(b1.momentum(), b2.momentum());
+        // console.log(`initial momentum ${p_initial}`);
+        // console.log(`final momentum ${p_final}`);
+
+      }
+    }
+  }
+
+}
---
+++ mover.js
@@ -0,0 +1,71 @@
+//creates ball objects when called w/ new
+function Mover(radius, loc, vel, acc, color){
+  this.radius = radius;
+  this.color = color;
+  //let volume = area and density = 1, so mass = area
+  this.mass = Math.PI * this.radius * this.radius;
+  this.loc = loc;
+  this.vel = vel;
+  this.acc = acc;
+
+  //track how many frames since 2 balls collided
+  this.collisionTracker = [];
+  for(var i = 0; i < num_balls; i++){
+    this.collisionTracker.push(10);
+  }
+  // create a threejs sphere with radius, widthSegments, heightSegments = 100
+  var geometry = new THREE.SphereGeometry( this.radius, 100, 100);
+
+  var material = new THREE.MeshLambertMaterial( { color: this.color } );
+  // The mesh is what threejs needs in the scene
+  this.mesh = new THREE.Mesh( geometry, material );
+  this.mesh.position.x = this.loc.x;
+  this.mesh.position.y = this.loc.y;
+}
+
+Mover.prototype.momentum = function(){
+  return vector2d.scalarMult(this.vel, this.mass);
+}
+
+Mover.prototype.kineticEnergy = function(){
+  var v = this.vel.magnitude();
+  return this.mass * v * v / 2;
+}
+
+//updates ball position
+Mover.prototype.update = function () {
+  this.checkEdges();
+  this.loc.add(this.vel);
+  this.vel.add(this.acc);
+  this.mesh.position.x = this.loc.x;
+  this.mesh.position.y = this.loc.y;
+}
+
+//reverses direction when ball hits edge
+Mover.prototype.checkEdges = function () {
+  if(this.loc.x + this.radius >= canvas.width){
+    this.loc.x = canvas.width - this.radius;
+    this.vel.x *= -1;
+  }
+  if(this.loc.x - this.radius < 0){
+    this.loc.x = this.radius;
+    this.vel.x *= -1;
+  }
+  if(this.loc.y + this.radius >= canvas.height){
+    this.loc.y = canvas.height - this.radius;
+    this.vel.y *= -1;
+  }
+  if(this.loc.y - this.radius < 0){
+    this.loc.y = this.radius;
+    this.vel.y *= -1;
+  }
+}
+
+// //draws ball
+// Mover.prototype.draw = function () {
+//   this.update();
+//   ctx.beginPath();
+//   ctx.arc(this.loc.x, this.loc.y, this.radius, 0, Math.PI * 2);
+//   ctx.fillStyle = this.color;
+//   ctx.fill();
+// };
